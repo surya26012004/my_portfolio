1562907b9b85482d9ec73b023144fb34707a83a5
surya <surya9393.k@gmail.com>
2025-02-18 19:15:21 +0530

    first

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/k--sravya/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/Sravya-212" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+        </a>
+
+        <a href="https://x.com/0xSravya" className="home__social-icon" target="_blank">
+        <i class="uil uil-twitter"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
