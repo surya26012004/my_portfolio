fa2e741482f9cac0fc12af349955de509781f5f2
surya <surya9393.k@gmail.com>
2025-03-05 19:36:37 +0530

    hyperlink changes

--- src/components/About/info.jsx
+++ src/components/About/info.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
-
 const Info = () => {
   return (
-    <div className="about__info grid">
-        <div className="about__box">
+    <div className="about__info grid">  <a href="#qualification">
+      <div className="about__box">
         <i class='bx bx-award about__icon'></i>
 
-            <h3 className="about__title">Experience</h3>
-                <span className="about__subtitle">7 months & working</span>
+        <h3 className="about__title">Experience</h3>
 
-        </div>
+        <span className="about__subtitle">7 months & working</span>
 
-        <div className="about__box">
+      </div>
+    </a>
+      <div className="about__box">
         <i class='bx bx-briefcase-alt about__icon'></i>
 
-            <h3 className="about__title">Completed</h3>
+        <h3 className="about__title">Completed</h3>
 
-                <span className="about__subtitle">5+ projects</span>
+        <span className="about__subtitle">5+ projects</span>
 
-        </div>
+      </div>
 
-
+      <a href="#contact">
         <div className="about__box">
-        <i class='bx bx-support about__icon'></i>
-            <h3 className="about__title">Support</h3>
-                <span className="about__subtitle">Contact 24/7</span>
+          <i class='bx bx-support about__icon'></i>
+          <h3 className="about__title">Contact Me</h3>
+          <span className="about__subtitle">Contact 24/7</span>
 
-        </div>
+        </div></a>
     </div>
   )
 }
